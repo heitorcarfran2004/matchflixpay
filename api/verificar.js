baf46015620cc2ccfb2843c187e2993472e488ef
heitorcarfran2004 <145309447+heitorcarfran2004@users.noreply.github.com>
2025-11-13 19:55:01 -0300

    Add files via upload

---
+++ api/verificar.js
@@ -0,0 +1,24 @@
+
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  const { txid } = req.query;
+
+  try {
+    const response = await fetch(`https://api.pushinpay.com/pix/${txid}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.PUSHIN_TOKEN}`
+      }
+    });
+
+    const data = await response.json();
+
+    return res.status(200).json({
+      ok: true,
+      status: data.pix.status
+    });
+
+  } catch (err) {
+    return res.status(500).json({ ok: false, error: err.toString() });
+  }
+}
